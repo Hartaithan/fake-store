c6d3617b23e76212fd84789180a7e01dfb6c111e
Hartaithan <hartaithan@gmail.com>
2023-08-31 18:55:30 +0900

    setup router

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,10 @@
 import { FC } from "react";
+import { RouterProvider } from "react-router-dom";
 import "./styles/global.css";
+import { router } from "./router";
 
 const App: FC = () => {
-  return <h1>Hello World!</h1>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
